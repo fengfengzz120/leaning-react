bf9dea8f3272de89fe89e9f1a6a5c9ee854eceb9
Xiao FengFeng <fengfengzz11220@stu.huel.edu.cn>
2025-05-12 15:13:51 +0800

    Create utils.ts

---
+++ utils.ts
@@ -0,0 +1,9 @@
+export const tryCatch = async <T = any>(
+  fn: Promise<T>
+): Promise<[Error] | [null, T]> => {
+  try {
+    return [null, await fn];
+  } catch (e: any) {
+    return [e || new Error("unknown error")];
+  }
+};
